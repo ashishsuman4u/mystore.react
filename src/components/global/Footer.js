6b0521ebad4ca32ac905d67ffec27bb8ce1287b2
Ashish Suman <ashishsuman4u@gmail.com>
2024-03-07 20:15:57 +0530

    Updated footer with static page links and removed google ad

--- src/components/global/Footer.js
+++ src/components/global/Footer.js
@@ -4,30 +4,45 @@ import { FaFacebookF, FaTwitter, FaInstagram, FaLinkedin } from 'react-icons/fa'
 function Footer() {
   return (
     <footer className="text-white bg-black body-font">
-      <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
-        <a className="flex title-font font-medium items-center md:justify-start justify-center text-white">
-          <img src="/favicon-32x32.png" alt="My Store" />
-          <span className="ml-3 text-xl">My Store</span>
-        </a>
-        <p className="text-sm text-white sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
-          &copy; {new Date().getFullYear()} My Store Pvt Ltd
-        </p>
-        <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
-          <a className="text-white">
-            <FaFacebookF />
-          </a>
-          <a className="ml-3 text-white">
-            <FaTwitter />
-          </a>
-          <a className="ml-3 text-white">
-            <FaInstagram />
-          </a>
-          <a className="ml-3 text-white">
-            <FaLinkedin />
-          </a>
-        </span>
+      <div className="container px-5 py-8 mx-auto">
+        <div className="flex sm:flex-row flex-col items-center justify-between gap-4">
+          <span className="flex sm:flex-row flex-col">
+            <a className="flex title-font font-medium items-center md:justify-start justify-center text-white">
+              <img src="/favicon-32x32.png" alt="My Store" />
+              <span className="ml-3 text-xl">My Store</span>
+            </a>
+            <p className="text-sm text-white sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-2">
+              &copy; {new Date().getFullYear()} My Store Pvt Ltd
+            </p>
+          </span>
+          <span className="flex">
+            <a className="ml-3 text-white" href="/support">
+              Support
+            </a>
+            <a className="ml-3 text-white" href="/privacy">
+              Privacy
+            </a>
+            <a className="ml-3 text-white" href="/terms">
+              Terms
+            </a>
+          </span>
+          <span className="flex">
+            <a className="text-white">
+              <FaFacebookF />
+            </a>
+            <a className="ml-3 text-white">
+              <FaTwitter />
+            </a>
+            <a className="ml-3 text-white">
+              <FaInstagram />
+            </a>
+            <a className="ml-3 text-white">
+              <FaLinkedin />
+            </a>
+          </span>
+        </div>
       </div>
-      <p className="text-center py-4">
+      <p className="text-center">
         Designed & Developed by{' '}
         <a rel="noreferrer" href="https://www.ashishsuman.co" target="_blank">
           Ashish Suman
--- src/pages/Static.js
+++ src/pages/Static.js
@@ -6,7 +6,7 @@ function Static({ type }) {
     __html: DOMPurify.sanitize(data[type].copy),
   });
   return (
-    <main className="static h-auto py-12 sm:py-16 lg:py-20 px-6">
+    <main className="static h-screen py-12 sm:py-16 lg:py-20 px-6">
       <h1 className="text-xl font-bold pb-6">{data[type].heading}</h1>
       <div dangerouslySetInnerHTML={sanitizedData()}></div>
     </main>
